43fda06148853ec258e60e9433e10b873d2792f0
Vadim Fedorov <bhoodream@yandex.ru>
2024-04-29 20:16:09 +0300

    part3 node project

--- part3/src/components/Alert/Alert.jsx
+++
@@ -1,4 +0,0 @@
-import "./styles.css";
-
-export const Alert = ({ type, message, visible }) =>
-  visible ? <div className={`alert alert__${type}`}>{message}</div> : null;
--- part3/src/hooks/useDataInvalidation.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-export const useDataInvalidation = () => {
-  const [date, setDate] = useState(() => Date.now());
-
-  const invalidate = () => setDate(Date.now());
-
-  return { date, invalidate };
-};
