33fddb0d62cb71e55f8e68061d082f8fb254c33d
Vadim Fedorov <bhoodream@yandex.ru>
2024-05-09 19:45:52 +0300

    fix

--- part3/models/person.js
+++ part3/models/person.js
@@ -12,9 +12,7 @@ const schema = new mongooseConnection.Schema(
       type: String,
       minLength: 8,
       validate: {
-        validator: function (v) {
-          return /^\d{2,3}-\d+$/.test(v);
-        },
+        validator: (v) => /^\d{2,3}-\d+$/.test(v),
         message: (props) => `${props.value} is not a valid phone number!`,
       },
       required: true,
